7b394caccdf4adbc4e29998e9f4dbf60d65f41cf
rodri <roroaltamore@gmail.com>
2024-10-17 20:55:27 +0000

    le puse 10 numeros al cosito este

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-nombre="rodri
-"
+nombre="rodri"
+numero=3
+booleano=True
+booleano=False
+print(numero)
+print(3)
+edadDeRodri=16
+edadparavotar=16
+PuedeVotar=edadDeRodri>=edadparavotar
+if PuedeVotar: True
+    print("Usted puede Votar")
+
